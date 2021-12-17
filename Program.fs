03fb10ac02e385d92e18f4f3854557004b43e6f2
Kevin <chgc.tw@gmail.com>
2021-12-17 20:38:33 +0800

    first version

--- Program.fs
+++ Program.fs
@@ -17,3 +17,44 @@
 // |Richard    |  100.00|  100.00|
 // |Sarah      |  100.00|  100.00|
 
+type Customer =
+    { Id: string
+      IsEligible: bool
+      IsRegistered: bool }
+
+let calculateTotal (customer: Customer) (spend: decimal) : decimal =
+    let discount =
+        if customer.IsEligible && spend >= 100M then
+            spend * 0.1M
+        else
+            0.0M
+
+    let total = spend - discount
+    total
+
+
+let john: Customer =
+    { Id = "John"
+      IsEligible = true
+      IsRegistered = true }
+
+let mary: Customer =
+    { Id = "Mary"
+      IsEligible = true
+      IsRegistered = true }
+
+let richard: Customer =
+    { Id = "Richard"
+      IsEligible = false
+      IsRegistered = false }
+
+let sarah: Customer =
+    { Id = "Sarah"
+      IsEligible = false
+      IsRegistered = false }
+
+
+let assertJohn = calculateTotal john 100.0M = 90.0M
+let assertMary = calculateTotal mary 99.0M = 99.0M
+let assertRichard = calculateTotal richard 100.0M = 100.0M
+let assertSarah = calculateTotal sarah 100.0M = 100.0M
